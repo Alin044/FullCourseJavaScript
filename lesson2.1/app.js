50c3a0e5348484bd88de5539d19f533096f9f8f9
Alin <alinalin03030@gmail.com>
2025-02-13 15:27:43 +0000

    auf

--- lesson2.1/app.js
+++ lesson2.1/app.js
@@ -12,3 +12,51 @@ setTimeout(() => {
         }, 2000);
     }, 2000);
 }, 2000);
+
+function user(name, age, work){
+    return {
+        name: name,
+        age: age, 
+        work: work,
+        intro: function(){
+            console.log(`Hello, I am ${name} and I am ${age}`);
+            console.log(`I am a ${work}`);
+        }
+    };
+};
+
+// function user(name, age, work){
+//     return {
+//         name,
+//         age, 
+//         work
+//     };
+// };
+//this method will still work the same
+
+
+
+const user1 = user("John", 24, "Programmer");
+user1.intro();
+console.log(user1);
+
+
+//The spread operators
+
+function giveMe4(a, b, c, d){
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+}
+
+const color = ["red", "green", "blue", "black"];
+
+giveMe4(...color) // this is the spread operator
+
+
+const strNums = ['one', 'two', 'tree'];
+const moreNum = ['four', 'five', 'six'];
+const concat = [...strNums, ... moreNum];
+
+console.log(concat);
